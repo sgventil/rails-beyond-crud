baf0d20edb3f8f284972e735c3d6104cb559e144
Fernando Ribeiro <pontogponto@gmail.com>
2023-07-29 09:59:59 +0200

    add chef name & top rating | model+views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants do
+    collection do
+      get :top
+      # get "top", to: "restaurants#top", as: :top_restaurants
+    end
+    member do
+      get :chef
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,8 @@ puts "Creating restaurants"
   restaurant = Restaurant.create(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_address,
-    rating: rand(1..5)
+    rating: rand(1..5),
+    chef_name: ["Alan Ducasse", "Clare Smyth", "Monica Galetti", "Jamie Oliver"].sample
   )
   puts "Restaurant with id #{restaurant.id} has been created"
 end
