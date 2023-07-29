00d03122a1d49a982c29547e0c4786b1e1165581
Fernando Ribeiro <pontogponto@gmail.com>
2023-07-29 09:17:19 +0200

    seed 100 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database"
+Restaurant.destroy_all
+
+puts "Creating restaurants"
+
+100.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    rating: rand(1..5)
+  )
+  puts "Restaurant with id #{restaurant.id} has been created"
+end
+
+puts "Finished!"
